1eccffb232b5eec4eaec6269905a1b4cb625a161
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2020-02-20 15:41:29 -0800

    got flat picker working and shaped up exercises table for production

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-//require("bootstrap-datepicker-rails")
 
 import "bootstrap"
 import "../stylesheets/application"
@@ -16,7 +15,7 @@ import flatpickr from "flatpickr"
 require("flatpickr/dist/flatpickr.css")
 
 document.addEventListener("turbolinks:load", () => {
-	flatpickr("data-behavior='flatpickr"), {
+	flatpickr("[data-behavior='flatpickr']"), {
 		dateFormat: "Y-m-d"
 	}
 })
