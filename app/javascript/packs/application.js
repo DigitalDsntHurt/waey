46cdec2557c25742ae699c0147125cca8a81f46d
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2020-02-22 15:22:56 -0800

    d3 barchart for winning consumption days per week

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+//require("packs/consumption_summaries.js.erb")
 
 import "bootstrap"
 import "../stylesheets/application"
