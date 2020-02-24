b4b78f8979e2065164d85ae88f66cd6026d0ecfc
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2020-02-24 08:48:56 -0800

    trying to solve scheduling issue for daily habits. deleted scheduler. Habits.create_daily_habits method now runs everytime we hit the today dashboard, which is the homepage. because of validations and uniqueness constraints, this shouldnt create any extra records and only successfully create records when it's the first time the page loads on a given day

--- config/initializers/daily_scheduler.rb
+++
@@ -1,9 +0,0 @@
-require 'rufus-scheduler'
-
-scheduler = Rufus::Scheduler.new
-
-scheduler.cron '1 0 * * *' do 
-  Habits.daily.each{|habit|
-    Daily.create(date: Date.today, habit: habit, done: false)
-  }
-end
