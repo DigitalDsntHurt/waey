d7b08cd23bba14670e92bc7f908d709cb767322b
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2020-03-02 08:56:53 -0800

    more last week review charts

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -4,13 +4,18 @@ class ReviewController < ApplicationController
 
   def last_week
     @journal = Daily.habit_metrics(Daily.filter_habit("journal",Daily.last_week))
-    @journal_chart_data = @journal.map{|date,bool| bool == true ? 1 : -1 }
+    @journal_chart_data = @journal.map{|date,bool| bool ? 1 : -1 }
 
     @meditate = Daily.habit_metrics(Daily.filter_habit("meditate",Daily.last_week))
+    @meditation_chart_data = @meditate.map{|date,bool| bool ? 1 : -1 }
+
     @visualize = Daily.habit_metrics(Daily.filter_habit("visualize success",Daily.last_week))
+    @visualize_chart_data = @visualize.map{|date,bool| bool ? 1 : -1 }
 
     @consumption = Consumption.win_loss_days_and_percents(Consumption.last_week)
+
     @alcohol = Daily.habit_metrics(Daily.filter_habit("no alcohol",Daily.last_week))
+    @alcohol_chart_data = @alcohol.map{|date,bool| bool ? 1 : -1 }
 
     @exercise = Exercise.review_totals
     @exercise_chart_data = Exercise.last_week_mins_per_day_chart_data
@@ -18,12 +23,13 @@ class ReviewController < ApplicationController
     @stretch = Daily.habit_metrics(Daily.filter_habit("stretch",Daily.last_week))
 
     @ambrush = Daily.habit_metrics(Daily.filter_habit("am brush",Daily.last_week))
-    @ambrush_chart_data = @ambrush.map{|date,bool| bool == true ? 1 : -1 }
+    @ambrush_chart_data = @ambrush.map{|date,bool| bool ? 1 : -1 }
 
     @pmbrush = Daily.habit_metrics(Daily.filter_habit("pm brush",Daily.last_week))
-    @pmbrush_chart_data = @pmbrush.map{|date,bool| bool == true ? 1 : -1 }
+    @pmbrush_chart_data = @pmbrush.map{|date,bool| bool ? 1 : -1 }
     
     @shower = Daily.habit_metrics(Daily.filter_habit("shower",Daily.last_week))
+    @shower_chart_data = @shower.map{|date,bool| bool ? 1 : -1 }
 
     @sleep = Sleep.review_totals
     @sleep_chart_data = Sleep.last_week_hrs_per_night_chart_data
