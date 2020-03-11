e0b3d42dd20d407c7911bf3c489d7aa200ac9802
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2020-03-11 08:20:11 -0700

    one step towards updating Habits.create_todays_habits method to create all outstanding habit records, even if most recent one isn't yesterday's

--- app/habits/habits.rb
+++ app/habits/habits.rb
@@ -3,6 +3,18 @@ class Habits
     ["no alcohol","stretch","journal","meditate","visualize success","shower","am brush","pm brush"]
   end
 
+  def self.check_most_recent_habit_date
+    @hsh = {}
+    Habits.daily.each{|habit|
+      @hsh[habit.gsub(" ","_").to_sym] = Daily.where(habit: habit).order(:date).last.date
+    }
+      @hsh[:exercise] = Exercise.all.last.date
+      @hsh[:sleep] = Sleep.all.last.date
+      @hsh[:hrv] = Hrv.all.last.date
+    
+    return @hsh
+  end
+
   def self.create_todays_habits
     Habits.daily.each{|habit|
       Daily.create(date: Date.today, habit: habit, done: false)
