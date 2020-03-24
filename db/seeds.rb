97334ba06d632e66485c76fad1ae1d0422197911
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2020-03-24 16:00:13 -0700

    changed nav drop-down link styles to not change on :visited

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,14 +24,8 @@ require 'csv'
 ##
 
 # AppTime.all_week_start_and_end_dates.each{|startdate,enddate|
-#   puts "~== #{startdate} ==~"
-#   p Sleep.total_hrs(startdate,enddate)
-#   puts "\n\n= = =\n\n"
+#   WeeklyDatum.create(week_of: startdate, meditation_days: Daily.completed_habits("meditate",startdate,enddate), journal_days: Daily.completed_habits("journal",startdate,enddate), winning_consumption_days: Consumption.winning_days(startdate,enddate), no_alcohol_days: Daily.completed_habits("no alcohol",startdate,enddate), exercise_days: Exercise.exercise_days(startdate,enddate), total_sleep_hours: Sleep.total_hrs(startdate,enddate), shower_days: Daily.completed_habits("shower",startdate,enddate), am_brush_days: Daily.completed_habits("am brush",startdate,enddate), pm_brush_days: Daily.completed_habits("pm brush",startdate,enddate))
 # }
-
-AppTime.all_week_start_and_end_dates.each{|startdate,enddate|
-  WeeklyDatum.create(week_of: startdate, meditation_days: Daily.completed_habits("meditate",startdate,enddate), journal_days: Daily.completed_habits("journal",startdate,enddate), winning_consumption_days: Consumption.winning_days(startdate,enddate), no_alcohol_days: Daily.completed_habits("no alcohol",startdate,enddate), exercise_days: Exercise.exercise_days(startdate,enddate), total_sleep_hours: Sleep.total_hrs(startdate,enddate), shower_days: Daily.completed_habits("shower",startdate,enddate), am_brush_days: Daily.completed_habits("am brush",startdate,enddate), pm_brush_days: Daily.completed_habits("pm brush",startdate,enddate))
-}
 
 ##
 ## ##
